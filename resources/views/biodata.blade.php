171bb06067f08d67165c90ecb859927b990827f6
alfadlilahilma <alfadlilahdela@gmail.com>
2023-11-06 16:13:46 +0700

    commit awal

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kenalan Yuk </title>
+    <!-- Tambahkan link ke file Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        /* Tambahkan CSS kustom di sini */
+        .biodata-container {
+            background-color: yellow;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        }
+        .profile-image {
+            width: 200px;
+            height: 200px;
+            object-fit: cover;
+            border: 5px solid #fff;
+            border-radius: 50%;
+        }
+    </style>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6 biodata-container">
+                <h1 class="text-center">Biodata Diri</h1>
+                <div class="text-center">
+                </div>
+                <p>Nama: ALFADLILAH ILMA</p>
+                <p>Umur: 20 tahun</p>
+                <p>Alamat: Jl. Letnan Ramli GG . 2 SUMENEP</p>
+                <p>Email: alfadlilahdela@gmail.com</p>
+                <p>No. Telepon: 087754078131</p>
+                <p>Tentang Saya: saya merupakan mahasiswa dari Universitas Trunojoyo Madura Prodi Pendidikan Informatika.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Tambahkan link ke file Bootstrap JavaScript (jQuery harus ada sebelum Bootstrap JS) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
